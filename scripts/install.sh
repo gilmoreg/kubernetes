5f579a66134bb67df33e09c8040d4d261b534032
Grayson Gilmore <gilmoreg@live.com>
2019-03-15 09:54:35 -0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,6 @@ snap install microk8s --classic
 microk8s.status --wait-ready
 microk8s.kubectl get nodes
 microk8s.kubectl get services
-snap alias microk8s.kubectl kubectl
+sudo snap alias microk8s.kubectl kubectl
 microk8s.enable dns ingress
 sudo iptables -P FORWARD ACCEPT
